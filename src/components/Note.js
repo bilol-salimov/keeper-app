64fee16a6d8296ebeecd5e325ef427c671aaadb4
bilol-salimov <salimovbilol321@gmail.com>
2023-07-13 22:39:13 +0500

    This is Keeper App first version

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = () => {
+    return (
+        <div className="note">
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, qui. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque magnam eligendi est nostrum commodi, perspiciatis recusandae ea maiores labore atque.</p>
+        </div>
+    )
+}
+
+export default Note;
